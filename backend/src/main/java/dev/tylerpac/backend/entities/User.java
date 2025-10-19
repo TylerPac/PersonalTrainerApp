b530453e7f01f9467e1cc48b6653e0bb39e356af
TylerPac <93409601+TylerPac@users.noreply.github.com>
2025-10-19 00:24:57 -0400

    ERD mapping for database/ Initail brainstorm for how we want fitness database to be, then will move on to nutrition

--- backend/src/main/java/dev/tylerpac/backend/entities/User.java
+++ backend/src/main/java/dev/tylerpac/backend/entities/User.java
@@ -1,9 +1,12 @@
 package dev.tylerpac.backend.entities;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class User {
@@ -18,36 +21,51 @@ public class User {
 
     private String password;
 
-    // Getters and setters
-    public Long getId() {
-        return id;
-    }
+    @OneToMany(mappedBy = "user")
+    private List<Nutrition> nutritionRecords;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    
+    @OneToMany(mappedBy = "user")
+    private List<Fitness> fitnessRecords;
 
-    public String getUsername() {
-        return username;
-    }
 
+    //sets
     public void setUsername(String username) {
         this.username = username;
     }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
     public void setPassword(String password) {
         this.password = password;
     }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public void setNutritionRecords(List<Nutrition> nutritionRecords) {
+        this.nutritionRecords = nutritionRecords;
+    }
+    public void setFitnessRecords(List<Fitness> fitnessRecords) {
+        this.fitnessRecords = fitnessRecords;
+    }
+    //gets
+    public String getUsername() {
+        return username;
+    }  
+    public String getPassword() {
+        return password;
+    }  
+    public String getEmail() {
+        return email;
+    }
+    public Long getId() {
+        return id;
+    }
+    public List<Nutrition> getNutritionRecords() {
+        return nutritionRecords;
+    }
+    public List<Fitness> getFitnessRecords() {
+        return fitnessRecords;
+    }
+    
 }
